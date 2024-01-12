68bc40d7f81bc9981ec37a94dc5299c8250960f6
nicolas.romanzin <nicolas.romanzin@thalesgroup.com>
2024-01-12 16:42:23 +0100

    1st acceptance criteria

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,7 +1,16 @@
-import { add } from './kata';
+import { isLeapYear } from './kata';
+
+/**
+ * - All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
+ * - All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
+ * - All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
+ * - All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019).
+ */
 
 describe('Kata', () => {
-  test('should get 4 when adding 2 and 2', () => {
-    expect(add(2, 2)).toBe(4);
+  test('All years divisible by 400 ARE leap years', () => {
+    expect(isLeapYear(1600)).toBe(true);
+    expect(isLeapYear(2000)).toBe(true);
+    expect(isLeapYear(2400)).toBe(true);
   });
 });
